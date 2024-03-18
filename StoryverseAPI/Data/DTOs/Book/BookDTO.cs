60cead4e655611ae636173b5746d7422c90528af
AzaKamala <azazuher@gmail.com>
2024-03-18 12:47:41 +0300

    Add Book DTOs

---
+++ StoryverseAPI/Data/DTOs/Book/BookDTO.cs
@@ -0,0 +1,20 @@
+﻿using StoryverseAPI.Data.Models;
+
+namespace StoryverseAPI.Data.DTOs.Book
+{
+    public class BookDTO
+    {
+        public BookDTO(Models.Book book)
+        {
+            Id = book.Id;
+            Title = book.Title;
+            Author = book.Author;
+            Chapters = book.Chapters;
+        }
+
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public Models.User Author { get; set; }
+        public List<Chapter> Chapters { get; set; }
+    }
+}
---
+++ StoryverseAPI/Data/DTOs/Book/CreateBookDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoryverseAPI.Data.DTOs.Book
+{
+    public class CreateBookDTO
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public long AuthorId { get; set; }
+    }
+}
